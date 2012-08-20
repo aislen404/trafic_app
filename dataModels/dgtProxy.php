62028e2e588f8e5c937b1d6e8642b9efbe76ab1a
Jose Samuel Berigüete Valero <aislen404@gmail.com>
2012-08-20 20:51:34 +0200

    2.1.0
    Finalmente un proxy

--- dataModels/dgtProxy.php
+++ dataModels/dgtProxy.php
@@ -1,26 +1,9 @@
 <?php
+	$codEle = $_GET['codEle'];
+	$tipo = $_GET['tipo'];
 
-	
-	$fltr_camaras = $_GET['Camaras'];
-	$fltr_eventos = $_GET['IncidenciasEVENTOS'];
-	$fltr_meteorologia = $_GET['IncidenciasMETEOROLOGICA'];
-	$fltr_obras = $_GET['IncidenciasOBRAS'];
-	$fltr_otros = $_GET['IncidenciasOTROS'];
-	$fltr_puertos = $_GET['IncidenciasPUERTOS'];
-	$fltr_retencion = $_GET['IncidenciasRETENCION'];
-	$fltr_paneles = $_GET['Paneles'];
-	$fltr_est_meteorologica = $_GET['SensoresMeteorologico'];
-	$fltr_trafico = $_GET['SensoresTrafico'];
-	$fltr_latNS = $_GET['latNS'];
-	$fltr_latSW = $_GET['latSW'];
-	$fltr_longNS = $_GET['longNS'];
-	$fltr_longSW = $_GET['longSW'];
-	$fltr_niveles = $_GET['niveles'];
-	$fltr_zoom = $_GET['zoom'];
+	$url='http://benalman.com/code/projects/php-simple-proxy/ba-simple-proxy.php?url=http%3A%2F%2Finfocar.dgt.es%2Fetraffic%2FBuscarElementosServlet%3Faccion%3DgetDetalles%26codEle%3D'.$codEle.'%26tipo%3D'.$tipo.'%26indiceMapa%3D0';
 
-	$url='http://infocar.dgt.es/etraffic/BuscarElementosServlet?latNS='.$fltr_latNS.'&longNS='.$fltr_longNS.'&latSW='.$fltr_latSW.'&longSW='.$fltr_longSW.'&zoom='.$fltr_zoom.'&accion=getElementos&Camaras='.$fltr_camaras.'&SensoresTrafico='.$fltr_trafico.'&SensoresMeteorologico='.$fltr_est_meteorologica.'&Paneles='.$fltr_paneles.'&IncidenciasRETENCION='.$fltr_retencion.'&IncidenciasOBRAS='.$fltr_obras.'&IncidenciasMETEOROLOGICA='.$fltr_meteorologia.'&IncidenciasPUERTOS='.$fltr_puertos.'&IncidenciasOTROS='.$fltr_otros.'&IncidenciasEVENTOS='.$fltr_eventos.'&niveles='.$fltr_niveles;
-
-	
 	$result = file_get_contents($url);
 	print $result;
 ?>
